affff33281b0ac8cbf61b3761be94eb7bd734cf9
Abu Desai <abhyuday.desai@gmail.com>
2022-12-25 15:16:11 -0800

    self.model save updated

--- app/algorithm/model/clustering.py
+++ app/algorithm/model/clustering.py
@@ -1,11 +1,10 @@
-
 import numpy as np, pandas as pd
 import joblib
 import sys
 import os, warnings
 from sklearn.cluster import SpectralClustering, KMeans
 
-warnings.filterwarnings('ignore') 
+warnings.filterwarnings("ignore")
 
 model_fname = "model.save"
 
@@ -13,61 +12,40 @@ MODEL_NAME = "clustering_base_spectral_clustering"
 
 
 class ClusteringModel:
-    
     def __init__(self, K, verbose=False, **kwargs) -> None:
-        self.K = K      
+        self.K = K
         self.verbose = verbose
         self.cluster_centers = None
         self.feature_names_in_ = None
-        
+
         self.model = self.build_model()
-        
-        
-    def build_model(self): 
+
+    def build_model(self):
         model = SpectralClustering(
-            n_clusters=self.K, 
+            n_clusters=self.K,
             verbose=self.verbose,
             eigen_solver="arpack",
             affinity="nearest_neighbors",
-            random_state=1
+            random_state=1,
         )
         return model
-    
 
-    def __getattr__(self, name):
-        # https://github.com/faif/python-patterns
-        # model.predict() instead of model.model.predict()
-        # same for fit(), transform(), fit_transform(), etc.
-        attr = getattr(self.model, name)
+    def fit_predict(self, *args, **kwargs):
+        return self.model.fit_predict(*args, **kwargs)
 
-        if not callable(attr): return attr
-
-        def wrapper(*args, **kwargs):
-            return getattr(self.model, attr.__name__)(*args, **kwargs)
-
-        return wrapper    
-    
-    
-    def save(self, model_path): 
-        joblib.dump(self.model, os.path.join(model_path, model_fname))
-
+    def save(self, model_path):
+        joblib.dump(self, os.path.join(model_path, model_fname))
 
     @classmethod
-    def load(cls, model_path):         
+    def load(cls, model_path):
         clusterer = joblib.load(os.path.join(model_path, model_fname))
         return clusterer
 
 
-def save_model(model, model_path):    
-    model.save(model_path) 
-    
+def save_model(model, model_path):
+    model.save(model_path)
 
-def load_model(model_path): 
-    try: 
-        model = ClusteringModel.load(model_path)        
-    except: 
-        raise Exception(f'''Error loading the trained {MODEL_NAME} model. 
-            Do you have the right trained model in path: {model_path}?''')
+
+def load_model(model_path):
+    model = ClusteringModel.load(model_path)
     return model
-
-
